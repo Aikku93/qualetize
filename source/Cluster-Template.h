54fc8d77a65ddbe57ac626d2bfd3c75695440bef
Aikku93 <aik@aol.com.au>
2024-10-12 21:38:45 +1100

    Split clusters globally rather than locally
    Instead of splitting a cluster's points over two clusters only, split the points out to all clusters on the next iteration. Also use a looser convergence for initialization to reduce the chances of overfitting.

--- source/Cluster-Template.h
+++ source/Cluster-Template.h
@@ -8,20 +8,13 @@ static void InsertAtListHead(uint32_t Idx, uint32_t *Head, uint32_t *List) {
 	*Head = Idx;
 }
 
-//! Remove element from linked list, given Prev and Next
-//! ie. this joins Prev and Next together in the list
-static void RemoveFromList(uint32_t Prev, uint32_t Next, uint32_t *Head, uint32_t *List) {
-	if(Prev != CLUSTER_END_OF_LIST) List[Prev] = Next;
-	else *Head = Next;
-}
-
 //! Add/remove a cluster in a distortion linked list (Head = Most distorted)
 static void InsertToDistortedClusterList(TCluster_t *Clusters, uint32_t Idx, uint32_t *Head) {
 	uint32_t Prev = CLUSTER_END_OF_LIST;
 	uint32_t Next = *Head;
-	float Dist = Clusters[Idx].MaxDistVal;
+	float Dist = Clusters[Idx].TotalDist;
 	if(Dist != 0.0f) {
-		while(Next != CLUSTER_END_OF_LIST && Dist < Clusters[Next].MaxDistVal) {
+		while(Next != CLUSTER_END_OF_LIST && Dist < Clusters[Next].TotalDist) {
 			Prev = Next;
 			Next = Clusters[Next].NextCluster;
 		}
@@ -36,8 +29,7 @@ static uint32_t PopDistortedClusterList(TCluster_t *Clusters, uint32_t *Head) {
 	return x;
 }
 
-//! ADd/remove a cluster in the "empty clusters" linked list
-//! NOTE: We don't use PrevCluster for this.
+//! Add/remove a cluster in the "empty clusters" linked list
 static void InsertToEmptyClusterList(TCluster_t *Clusters, uint32_t Idx, uint32_t *Head) {
 	Clusters[Idx].NextCluster = *Head;
 	*Head = Idx;
@@ -75,86 +67,22 @@ static uint8_t ResolveCluster(
 	TCluster_ResolveCentroid(Cluster, nDims);
 
 	//! Recalculate distortion
-	//! Note that MaxDistVal stores the average between the average
-	//! distortion, and the peak distortion. This generally reduces
-	//! the chances of overfitting, while still giving a clearer idea
-	//! of which cluster needs splitting most urgently.
-	uint32_t Prev = CLUSTER_END_OF_LIST, Next = Cluster->FirstDataIdx;
-	uint32_t PeakDistIdx = CLUSTER_END_OF_LIST, PeakDistIdxPrev = CLUSTER_END_OF_LIST;
+	uint32_t Next = Cluster->FirstDataIdx;
+	uint32_t PeakDistIdx = CLUSTER_END_OF_LIST;
 	float    PeakDistVal = 0.0f;
 	float    TotalDist   = 0.0f;
 	while(Next != CLUSTER_END_OF_LIST) {
 		float Dist = TCluster_Dist2ToCentroid(Cluster, Data + Next*nDims, nDims);
 		if(Dist > PeakDistVal) {
-			PeakDistIdx = Next, PeakDistIdxPrev = Prev;
+			PeakDistIdx = Next;
 			PeakDistVal = Dist;
 		}
 		TotalDist += Dist;
-		Prev = Next;
 		Next = ClusterListIndices[Next];
 	}
-	Cluster->MaxDistIdx = PeakDistIdx, Cluster->MaxDistIdxPrev = PeakDistIdxPrev;
-	Cluster->MaxDistVal = PeakDistVal + (TotalDist / (float)nPoints);
+	Cluster->MaxDistIdx = PeakDistIdx;
 	Cluster->TotalDist  = TotalDist;
 	return 1;
-}
-
-//! Split cluster into two separate clusters
-static void SplitCluster(
-	uint32_t SrcClusterIdx,
-	uint32_t DstClusterIdx,
-	TCluster_t *Clusters,
-	const TClusterData_t *Data,
-	uint32_t *ClusterListIndices,
-	uint32_t *DistClusterHead,
-	uint32_t nDims
-) {
-	TCluster_t *SrcCluster = &Clusters[SrcClusterIdx];
-	TCluster_t *DstCluster = &Clusters[DstClusterIdx];
-
-	//! Create a new cluster from the most distorted point - this helps
-	//! us make it out of a local optimum into a better cluster fit.
-	TCluster_SetCentroidToData(DstCluster, Data + SrcCluster->MaxDistIdx*nDims, nDims);
-	DstCluster->FirstDataIdx = CLUSTER_END_OF_LIST;
-	ClearTraining(DstCluster, nDims);
-
-	//! Now re-cluster the data points that fell into the source cluster
-	uint32_t Prev = CLUSTER_END_OF_LIST;
-	uint32_t Next = SrcCluster->FirstDataIdx;
-	ClearTraining(SrcCluster, nDims);
-	while(Next != CLUSTER_END_OF_LIST) {
-		uint32_t Idx = Next;
-		Next = ClusterListIndices[Idx];
-
-		//! Decide whether to use the original cluster or the newly-created one
-		const TClusterData_t *ThisData = Data + Idx*nDims;
-		float DistSrc = TCluster_Dist2ToCentroid(SrcCluster, ThisData, nDims);
-		float DistDst = TCluster_Dist2ToCentroid(DstCluster, ThisData, nDims);
-		if(DistSrc < DistDst) {
-			Prev = Idx;
-			TrainCluster(SrcCluster, ThisData, nDims);
-		} else {
-			//! Remove from SrcCluster list before inserting to DstCluster list
-			RemoveFromList(Prev, Next, &SrcCluster->FirstDataIdx, ClusterListIndices);
-			InsertAtListHead(Idx, &DstCluster->FirstDataIdx, ClusterListIndices);
-			TrainCluster(DstCluster, ThisData, nDims);
-		}
-	}
-
-	//! Resolve the new cluster centroids
-	//! NOTE: Do not re-insert these clusters into the distortion
-	//! list, or we end up "prematurely optimizing" the result,
-	//! causing technically lower RMSE, at the cost of nasty
-	//! overfitting artifacts (where the algorithm will assign
-	//! very few points to a cluster, even if that increases the
-	//! error for other clusters significantly).
-	(void)DistClusterHead;
-	if(ResolveCluster(SrcCluster, Data, ClusterListIndices, nDims)) {
-		//InsertToDistortedClusterList(Clusters, SrcClusterIdx, DistClusterHead);
-	}
-	if(ResolveCluster(DstCluster, Data, ClusterListIndices, nDims)) {
-		//InsertToDistortedClusterList(Clusters, DstClusterIdx, DistClusterHead);
-	}
 }
 
 /************************************************/
@@ -186,49 +114,17 @@ static inline uint32_t TClusterize_Process(
 	ResolveCluster(&Clusters[0], Data, ClusterListIndices, nDims);
 	InsertToDistortedClusterList(Clusters, 0, &DistClusterHead);
 
-	//! Now begin splitting the most distorted clusters until we
-	//! have the desired number of output clusters. Note that
-	//! we only enter this loop if we had any distortion when
-	//! we calculated the first cluster (otherwise we've already
-	//! achieved perfect results and don't need to split).
+	//! Begin creating additional clusters
 	float LastSplitDist = Clusters[0].TotalDist;
-	if(LastSplitDist != 0.0f) while(nCurrentClusters < nClusters) {
-		//! Split the most distorted clusters out
-		//! We can split as many times as we want here, with
-		//! N=1..nClusters-1. In general, N=1 gives the best
-		//! results, but can take absurd amounts of time when
-		//! nClusters is large, whereas N=nClusters-1 still
-		//! gives good results, just somewhat sub-optimal as
-		//! compared to N=1 (-1dB PSNR ish), but is faster.
-		uint32_t nNewClustersRem = nCurrentClusters;
-		if(nCurrentClusters+nNewClustersRem > nClusters) nNewClustersRem = nClusters - nCurrentClusters;
-		while(nNewClustersRem) {
-			//! Find a distorted cluster to split from
-			uint32_t SrcCluster;
-			if(DistClusterHead != CLUSTER_END_OF_LIST) {
-				SrcCluster = PopDistortedClusterList(Clusters, &DistClusterHead);
-			} else break;
-
-			//! If we have no empty clusters, then generate a new one
-			uint32_t DstCluster;
-			if(EmptyClusterHead != CLUSTER_END_OF_LIST) {
-				DstCluster = PopEmptyClusterList(Clusters, &EmptyClusterHead);
-			} else {
-				DstCluster = nCurrentClusters;
-				nCurrentClusters++;
-				nNewClustersRem--;
-			}
-
-			//! Actually split the cluster
-			SplitCluster(
-				SrcCluster,
-				DstCluster,
-				Clusters,
-				Data,
-				ClusterListIndices,
-				&DistClusterHead,
-				nDims
-			);
+	while(nCurrentClusters < nClusters && DistClusterHead != CLUSTER_END_OF_LIST) {
+		//! Create new cluster from the most distorted data point
+		//! Note that we are splitting out the most distorted point
+		//! of the most distorted cluster, NOT the most distorted
+		//! point general. This is an important distinction.
+		{
+			uint32_t SrcCluster = PopDistortedClusterList(Clusters, &DistClusterHead);
+			uint32_t DstCluster = nCurrentClusters++;
+			TCluster_SetCentroidToData(&Clusters[DstCluster], Data + Clusters[SrcCluster].MaxDistIdx*nDims, nDims);
 		}
 
 		//! Begin refinement loop
@@ -255,41 +151,36 @@ static inline uint32_t TClusterize_Process(
 			}
 
 			//! Resolve all clusters
+			float ThisPassDist = 0.0f;
 			DistClusterHead  = CLUSTER_END_OF_LIST, DistClusterHead = CLUSTER_END_OF_LIST;
 			EmptyClusterHead = CLUSTER_END_OF_LIST;
 			for(k=0;k<nCurrentClusters;k++) {
 				if(ResolveCluster(&Clusters[k], Data, ClusterListIndices, nDims)) {
 					//! If the cluster resolves, update the distortion linked list
 					InsertToDistortedClusterList(Clusters, k, &DistClusterHead);
+					ThisPassDist += Clusters[k].TotalDist;
 				} else {
 					//! No resolve - append to empty-cluster linked list
 					InsertToEmptyClusterList(Clusters, k, &EmptyClusterHead);
 				}
 			}
 
-			//! Split the most distorted clusters into any empty ones
+			//! Split the most distorted data points into empty clusters
 			while(DistClusterHead != CLUSTER_END_OF_LIST && EmptyClusterHead != CLUSTER_END_OF_LIST) {
 				uint32_t SrcCluster = PopDistortedClusterList(Clusters, &DistClusterHead);
 				uint32_t DstCluster = PopEmptyClusterList    (Clusters, &EmptyClusterHead);
-				SplitCluster(
-					SrcCluster,
-					DstCluster,
-					Clusters,
-					Data,
-					ClusterListIndices,
-					&DistClusterHead,
-					nDims
-				);
+				TCluster_SetCentroidToData(&Clusters[DstCluster], Data + Clusters[SrcCluster].MaxDistIdx*nDims, nDims);
 			}
 
-			//! Calculate global distortion and early exit
-			float ThisPassDist = 0.0f;
-			for(k=0;k<nCurrentClusters;k++) ThisPassDist += Clusters[k].TotalDist;
-			ThisPassDist /= (float)nDataPoints;
-			if(ThisPassDist == 0.0f || ThisPassDist >= LastPassDist) {
-				LastPassDist = ThisPassDist;
-				break;
-			}
+			//! If we're still in the initialization stage, stop once we
+			//! are getting too close to convergence, as we do not want
+			//! this to happen just yet.
+			if(nCurrentClusters < nClusters && ThisPassDist > 0.95f*LastPassDist) Pass = nPasses-1;
+
+			//! Early exit on convergence
+			if(ThisPassDist == 0.0f || ThisPassDist >= LastPassDist) Pass = nPasses-1;
+
+			//! Set up for next iteration
 			LastPassDist = ThisPassDist;
 		}
 
